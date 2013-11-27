d685fbaa09f0830449e0122acb4faa616e5f64a1
Garrett Richards <grichards@trulia.com>
2013-11-27 15:05:04 -0800

    changed bundle

--- FontAwesomeKit/FAKFoundationIcons.m
+++ FontAwesomeKit/FAKFoundationIcons.m
@@ -9,7 +9,7 @@
 #ifndef DISABLE_FOUNDATIONICONS_AUTO_REGISTRATION
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        [self registerIconFontWithURL: [[NSBundle mainBundle] URLForResource:@"foundation-icons" withExtension:@"ttf"]];
+        [self registerIconFontWithURL: [[NSBundle coreResourcesBundle] URLForResource:@"foundation-icons" withExtension:@"ttf"]];
     });
 #endif
     
